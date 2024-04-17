715bfbcf64e86a7124ccbe30c47dab0eff921415
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2024-04-17 16:40:27 +0300

    fav and nav icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,6 @@
 // "use client";
+import Image from "next/image";
+import Logo from "../public/fav.png";
 import Link from "next/link";
 // import { useRouter } from "next/navigation";
 import React from "react";
@@ -6,8 +8,11 @@ import React from "react";
 const Navbar = () => {
   // const {} = useRouter();
   return (
-    <div>
-      <p>Navigation</p>
+    <div className="flex">
+      <Image src={Logo} alt="Logo" height={50} width={50} priority />
+      <div>
+        <Link href={"/progjects"}>Projects</Link>
+      </div>
     </div>
   );
 };
