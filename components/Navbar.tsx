b23a1fcbc0600519bda7852ac9a14e32e2aaa0be
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2024-04-17 16:40:14 +0300

    favicon and navbar icon

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+// "use client";
+import Link from "next/link";
+// import { useRouter } from "next/navigation";
+import React from "react";
+
+const Navbar = () => {
+  // const {} = useRouter();
+  return (
+    <div>
+      <p>Navigation</p>
+    </div>
+  );
+};
+
+export default Navbar;
